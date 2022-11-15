35d3675c91206636ff18f741fcd8fa5c7a96344c
NiteshKadianFE <nitesh.kadian@getfareye.com>
2022-11-15 16:26:28 +0530

    login signup api

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {login} from './api/Login';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import DrawerNavigator from './navigation/DrawerNavigator';
-import {AdminAllUsersPage, AdminBlueTickRequestPage, EditProfilePage, Login, Signup} from './pages/index';
+import {AdminAllUsersPage, AdminBlueTickRequestPage, Login, Signup} from './pages/index';
 import {AsyncStorageConstants} from './constants/AsyncStorageConstants';
 import AdminDrawerNavigator from './navigation/AdminDrawerNavigator';
 
@@ -21,15 +21,14 @@ export default function App() {
     setIsLoggedIn(isSuccessful);
     setIsLoading(false);
   }
-
+  
   useEffect(() => {
     handleLogin();
   }, []);
 
   return (
     <>
-    <Signup />
-      {/* {isLoading ? (
+      {isLoading ? (
         <SafeAreaView>
           <Image source={LoadingImage} style={styles.loadingImage} />
         </SafeAreaView>
@@ -39,7 +38,7 @@ export default function App() {
         <NavigationContainer>
           <DrawerNavigator />
         </NavigationContainer>
-      )} */}
+      )}
     </>
   );
 }
