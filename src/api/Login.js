41c7c2bc2528c25987a41cf5b7a22694e086c802
Kushagra Singh <kushagra.singh@getfareye.com>
2022-11-15 17:12:26 +0530

    updating login api

--- src/api/Login.js
+++ src/api/Login.js
@@ -4,9 +4,7 @@ import Axios from './Axios';
 
 export const login = async data => {
   console.log(data);
-  const xy = await Axios.post(
-    `/login?username=foo&password=bar`,
-  )
+  const xy = await Axios.post(`/login?username=foo&password=bar`)
     .then(res => {
       console.log(res.data);
     })
@@ -18,23 +16,19 @@ export const login = async data => {
     'JSESSIONID=F3002E8F28A3B75976111B62942D08F6',
   );
   await AsyncStorage.setItem(AsyncStorageConstants.USER_DETAILS, 'userObject');
-  return new Promise(resolve => setTimeout(resolve, 100, true));
+  return new Promise(resolve => setTimeout(resolve, 100, false));
 };
 
 export const signUp = async user => {
   let usertemp = {
-    "dob": "2020-11-15T05:05:39.061Z",
-      "name": "kada",
-       "password": "kada",
-        "userName": "Kada"
-        }
+    dob: '2020-11-15T05:05:39.061Z',
+    name: 'kada',
+    password: 'kada',
+    userName: 'Kada',
+  };
 
-  console.log("sssssssssssaaaaa",usertemp);
-  console.log("sssssssssssaaaaa",user);
-
-  Axios.post('/user', user["user"])
-    .then(res => console.log("ssssssss",res.data))
-    .catch((error) => console.log( error.response.request._response ) );
-  // console.log(res);
+  Axios.post('/user', user['user'])
+    .then(res => console.log('ssssssss', res.data))
+    .catch(error => console.log(error.response.request._response));
   return new Promise(resolve => setTimeout(resolve, 100, true));
 };
